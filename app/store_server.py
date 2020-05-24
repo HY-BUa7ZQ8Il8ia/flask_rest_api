5042d47175e844abb1ed0870b4a9a239f43e979f
SH <SH@example.com>
2020-05-24 22:10:42 +0900

    fix store_server.py

---
+++ app/store_server.py
@@ -0,0 +1,81 @@
+from flask import Flask
+from flask import jsonify
+from flask import render_template
+from flask import request
+
+app = Flask(__name__, template_folder='./templates')
+
+
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+
+@app.route('/')
+def index():
+    return render_template('./index.html')
+
+
+@app.route('/store', methods=['GET'])
+def get_stores():
+    return jsonify({'stores': stores})
+
+
+@app.route('/store/<string:name>', methods=['GET'])
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+
+@app.route('/store/<string:name>/item', methods=['GET'])
+def get_item_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'store not found'})
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['item'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=8000,
+                        type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
